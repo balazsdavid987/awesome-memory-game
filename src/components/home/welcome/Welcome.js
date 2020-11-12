fd672c2d301cb81e7521da628964978cf0f33e20
balazsdavid987 <balazsdavid987@gmail.com>
2020-11-12 19:26:41 +0100

    refactor Welcome component

--- src/components/home/welcome/Welcome.js
+++ src/components/home/welcome/Welcome.js
@@ -1,12 +1,47 @@
+import { useHistory } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+
+import { startNewGame } from "../../../actions";
+
 import styles from "./Welcome.module.scss";
 
-import StartNewGame from "../../start-new-game/StartNewGame";
+import Button from "../../button/Button";
+
+import { createDeck } from "../../../utils";
+import { cardTypes } from "../../../constants";
 
 const Welcome = () => {
+  const dispatch = useDispatch();
+  const deckSize = useSelector((state) => state.game.deckSize);
+  const currentDeck = useSelector((state) => state.game.currentDeck);
+  const history = useHistory();
+
+  const onNewGameClicked = () => {
+    const deck = createDeck(cardTypes, deckSize);
+    dispatch(startNewGame(deck));
+
+    history.push("/play");
+  };
+
+  const onContinueGameClicked = () => {
+    history.push("/play");
+  };
+
   return (
     <section>
       <h1 className={styles.title}>Awesome Memory Game</h1>
-      <StartNewGame />
+      <Button
+        label="Start new game"
+        type="primary"
+        onClick={onNewGameClicked}
+      />
+      {currentDeck !== undefined && currentDeck.length !== 0 && (
+        <Button
+          label="Continue game"
+          type="secondary"
+          onClick={onContinueGameClicked}
+        />
+      )}
     </section>
   );
 };
