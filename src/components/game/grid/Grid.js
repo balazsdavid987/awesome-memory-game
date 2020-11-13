3771e8aae26fae76a67408a6649ef61716fdd472
balazsdavid987 <balazsdavid987@gmail.com>
2020-11-13 12:52:43 +0100

    add disabled prop to Grid

--- src/components/game/grid/Grid.js
+++ src/components/game/grid/Grid.js
@@ -1,7 +1,14 @@
 import styles from "./Grid.module.scss";
 
-const Grid = ({ children }) => {
-  return <div className={styles.component}>{children}</div>;
+const Grid = ({ children, disabled }) => {
+  return (
+    <div
+      style={{ pointerEvents: disabled ? "none" : "initial" }}
+      className={styles.component}
+    >
+      {children}
+    </div>
+  );
 };
 
 export default Grid;
